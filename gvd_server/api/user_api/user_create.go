c5180d6ec1b5814562f5317798acd0b5e7f4a95e
wwwwwwwwt <1162933894@qq.com>
2023-07-09 10:44:26 +0800

    finish userCreate

--- gvd_server/api/user_api/user_create.go
+++ gvd_server/api/user_api/user_create.go
@@ -2,12 +2,13 @@
  * @Author: zzzzztw
  * @Date: 2023-07-04 22:06:09
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-07-08 12:35:07
+ * @LastEditTime: 2023-07-09 10:40:20
  * @FilePath: /gvdoc/gvd_server/api/user_api/user_create.go
  */
 package userapi
 
 import (
+	"fmt"
 	"gvd_server/global"
 	"gvd_server/models"
 	"gvd_server/service/common/res"
@@ -39,7 +40,18 @@ func (UserApi) UserCreateView(c *gin.Context) {
 		res.FailWithMsg("用户名已经存在", c)
 		return
 	}
-
+	if cr.NickName == "" {
+		// 如果昵称不存在，
+		var maxID uint
+		global.DB.Model(models.UserModel{}).Select("max(id)").Scan(&maxID)
+		cr.NickName = fmt.Sprintf("用户_%d", maxID+1)
+	}
+	var role models.RoleDocModel
+	err = global.DB.Take(&role, cr.RoleID).Error
+	if err != nil {
+		res.FailWithMsg("角色不存在", c)
+		return
+	}
 	err = global.DB.Create(&models.UserModel{
 		UserName: cr.UserName,
 		Password: pwd.HashPwd(cr.Password),
